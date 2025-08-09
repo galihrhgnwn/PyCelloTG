57b3748f0732e083bbbf862b63a9cb9cf3916edb
cello <114824928+galihrhgnwnnn@users.noreply.github.com>
2025-08-09 13:57:25 +0700

    Update aisearchsong.py

--- utils/aisearchsong.py
+++ utils/aisearchsong.py
@@ -2,21 +2,22 @@
 
 import g4f
 
-PROVIDER = g4f.Provider.Blackbox
-MODEL = "gpt-4o"
+PROVIDER = g4f.Provider.DeepInfra
+MODEL = "meta-llama/Llama-4-Scout-17B-16E-Instruct"
 
 async def generate_ai_slang(title: str, artist_names: str, time: float) -> str:
     prompt = (
-    f"Explain the meaning of the song '{title}' by {artist_names}' "
-    "in just one creative sentence using casual American slang, abbreviations, and expressions (like tbh, ngl, fr, etc). "
-    "Do NOT say things like 'hits hard' or 'gives the feels'. Avoid repeating phrases. Be original, fun, and brief."
-)
+        f"Explain the meaning of the song '{title}' by {artist_names} "
+        "in one concise and creative sentence in American English, "
+        "and include relevant emojis that match the song's meaning."
+    )
 
     try:
         response = g4f.ChatCompletion.create(
             model=MODEL,
             provider=PROVIDER,
             messages=[{"role": "user", "content": prompt}],
+            web_search=True,  # aktifin web search
             stream=False
         )
 
