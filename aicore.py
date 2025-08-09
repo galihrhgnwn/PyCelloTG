3e9db0fd24d15d7b07447f87d48835b4c43bec56
cello <114824928+galihrhgnwnnn@users.noreply.github.com>
2025-08-09 15:33:50 +0700

    Update aicore.py

--- aicore.py
+++ aicore.py
@@ -44,7 +44,6 @@ class AICore:
             "provider": "DeepInfra",
             "model": self.model_name,
             "messages": messages,
-            "web_search": True,
             "temperature": 0.8,
             "max_tokens": 1500
         }
